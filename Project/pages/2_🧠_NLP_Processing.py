c43f567e1110d0e98f7bff4fb8c9f787c6de5184
Rameesha Kankanamge <rameeshakankanamge@gmail.com>
2026-07-08 09:39:38 +0530

    Update 2_🧠_NLP_Processing.py

--- Project/pages/2_🧠_NLP_Processing.py
+++ Project/pages/2_🧠_NLP_Processing.py
@@ -6,7 +6,7 @@ import json
 st.set_page_config(page_title="NLP Features", page_icon="🧠", layout="wide")
 st.title("🧠 Natural Language Processing Tokenizer View")
 
-conn = sqlite3.connect("/content/Project/database/requirements.db")
+conn = sqlite3.connect("Project/database/requirements.db")
 df = pd.read_sql_query("SELECT nlp_id, requirement_id, cleaned_text FROM NLPResults", conn)
 conn.close()
 
