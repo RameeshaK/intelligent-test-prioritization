70a38c36f4aef5bd589c3f1bf7874da18be54811
Rameesha Kankanamge <rameeshakankanamge@gmail.com>
2026-07-08 09:40:16 +0530

    Update 5_⭐_Prioritization.py

--- Project/pages/5_⭐_Prioritization.py
+++ Project/pages/5_⭐_Prioritization.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.set_page_config(page_title="Prioritization Queue", page_icon="⭐", layout="wide")
 st.title("⭐ Optimally Prioritized Execution Queue")
 
-conn = sqlite3.connect("/content/Project/database/requirements.db")
+conn = sqlite3.connect("Project/database/requirements.db")
 # Fetch ranked items
 df = pd.read_sql_query("SELECT final_rank, calculated_priority_score, test_scenario, test_case_type FROM GeneratedTestCases ORDER BY final_rank ASC", conn)
 conn.close()
